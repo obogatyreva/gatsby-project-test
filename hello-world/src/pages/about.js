08fd9b2c0c494e3b24d4a462076b54e612c35975
Olga Bogatyreva <helga_bogatyreva@mail.ru>
2019-07-30 19:39:56 +0300

    feat: add a simple gatsby-site

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="Pretty cool" />
+    <p>What a world.</p>
+  </div>
+)
